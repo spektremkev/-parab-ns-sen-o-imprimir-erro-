d7848816c417178cb9accec714735319da13515d
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2020-10-04 17:07:24 -0300

    Add files via upload

---
+++ “parabéns”, senão imprimir “erro”.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+/*atividade 1_questão 3
+		*/
+
+
+int main()<%
+	
+   int numero;
+   	
+	printf("Escreva uma valor: ");
+	  scanf("%d",&numero);
+	  
+	  if(numero < 10)
+	  	cout <<("ERRO");
+	  	
+		 if((numero>= 10) and (numero <= 50))
+		 	cout <<("Parabens");
+	 	
+			 if(numero > 50)
+			 	cout <<("ERRO");		
+%>
